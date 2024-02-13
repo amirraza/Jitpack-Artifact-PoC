29cf4c5e3c71344dbc437c1f6a5b783ddf2879f9
AmirRaza <amir.raza@purevpn.com>
2024-02-13 16:52:12 +0500

    Add afterEvaluate for publishing

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -42,3 +42,14 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                version = project.version.toString()
+            }
+        }
+    }
+}
