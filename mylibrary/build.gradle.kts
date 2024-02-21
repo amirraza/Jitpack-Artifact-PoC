54e8ff9db0ab74a9e2d2fd2154cbbe8526200afa
AmirRaza <amir.raza@purevpn.com>
2024-02-21 17:14:04 +0500

    getByName("release")

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
