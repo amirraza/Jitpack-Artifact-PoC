deedd01cf35308704de47c55c49e19c48be7e98f
AmirRaza <amir.raza@purevpn.com>
2024-02-23 19:34:22 +0500

    tasks.whenTaskAdded

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -94,11 +94,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
-//tasks.whenTaskAdded {
-//    if(this.name.endsWith("Jar")) {
-//        this.enabled = false
-//    }
-//}
+tasks.whenTaskAdded {
+    if(name.contains("Source", ignoreCase = false)) {
+        enabled = false
+    }
+}
 //tasks.register<Jar>("sourcesJar") {
 //    archiveClassifier.set("sources")
 //    from(android.sourceSets["main"].java.srcDirs)
